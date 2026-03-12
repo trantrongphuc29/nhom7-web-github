9d659f6e6f75da04d40e126424233040cddeb781
Trần Trọng Phúc <trongphuc636@gmail.com>
2026-03-12 14:29:21 +0700

    fix: update users API CORS and htaccess for deploy

--- BackendWeb/api/users/index.php
+++ BackendWeb/api/users/index.php
@@ -1,5 +1,8 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require_once dirname(__DIR__, 2) . '/config/database.php';
 require_once dirname(__DIR__, 2) . '/models/User.php';
