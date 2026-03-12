841a439c470055163c6373a6b94a2e7493294eba
Trần Trọng Phúc <trongphuc636@gmail.com>
2026-03-12 18:24:50 +0700

    fix

--- BackendWeb/config/database.php
+++ BackendWeb/config/database.php
@@ -1,9 +1,9 @@
 <?php
 class Database {
-    private $host = "sql106.infinityfree.com";
-    private $db_name = "if0_41294787_labstore_db";
-    private $username = "if0_41294787";
-    private $password = "OHbqhgAnUuF";
+    private $host = "sql105.infinityfree.com";
+    private $db_name = "if0_41296920_labstore_db";
+    private $username = "if0_41296920";
+    private $password = "22122004Nhi";
     public $conn;
 
     public function getConnection() {
