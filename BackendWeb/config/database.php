32f616916731cb7cbdf3295b150e40418d5a8654
Trần Trọng Phúc <trongphuc636@gmail.com>
2026-03-13 16:02:58 +0700

    fix

--- BackendWeb/config/database.php
+++ BackendWeb/config/database.php
@@ -30,7 +30,6 @@ class Database {
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES => false,
-                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
                 ]
             );
 
